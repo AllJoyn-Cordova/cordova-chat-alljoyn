8a3446356c8fe6fe4db8f3cfd441ade93159a6f2
Ville Rantala <ville.rantala@iki.fi>
2014-12-08 09:35:08 +0200

    Fix popover location on Internet Explorer.
    On IE, document.body.clientWidth reported 0, which resulted into
    popover being rendered outside of the viewport. Using innerWidth
    instead fixes the issue on IE and seems to work okay also on iOS
    and Android.

--- www/lib/ionic/js/ionic.bundle.js
+++ www/lib/ionic/js/ionic.bundle.js
@@ -37482,8 +37482,10 @@ function($ionicModal, $ionicPosition, $document, $window) {
     var buttonOffset = $ionicPosition.offset( targetEle );
     var popoverWidth = popoverEle.prop('offsetWidth');
     var popoverHeight = popoverEle.prop('offsetHeight');
-    var bodyWidth = $document[0].body.clientWidth;
-    // clientHeight doesn't work on all platforms for body
+
+    // Use innderWidth and innerHeight, because clientWidth and clientHeight
+    // doesn't work consistently for body on all platforms
+    var bodyWidth = $window.innerWidth;
     var bodyHeight = $window.innerHeight;
 
     var popoverCSS = {
