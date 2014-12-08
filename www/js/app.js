6216e31c261900d4b289086d4e2090aef8a1405c
Ville Rantala <ville.rantala@iki.fi>
2014-12-08 13:05:07 +0200

    Don't try to hide the keyboard accessory bar.
    The keyboard plugin is not yet implemented for Windows and using
    it makes the app crash.

--- www/js/app.js
+++ www/js/app.js
@@ -2,11 +2,6 @@ var chatApp = angular.module('chatApp', ['ionic']);
 
 chatApp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
