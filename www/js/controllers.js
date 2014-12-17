1adc0eafe99b9f53976a3d2b6242cbf9380810d1
Ville Rantala <ville.rantala@iki.fi>
2014-12-17 15:51:59 +0200

    Guard event handlers to avoid duplicate event handling.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,30 +7,34 @@ chatApp.controller('HeaderController', function($rootScope, $scope, $ionicPopove
     $scope.popover = popover;
   });
   $scope.openChannelSelector = function($event) {
-    $scope.loading = true;
-    $scope.popover.show($event);
-    chatService.getChannels().then(function(channels) {
-      $scope.loading = false;
-      $scope.channels = channels;
-    });
+    if ($event.isIonicTap) {
+      $scope.loading = true;
+      $scope.popover.show($event);
+      chatService.getChannels().then(function(channels) {
+        $scope.loading = false;
+        $scope.channels = channels;
+      });
+    }
   };
 
   $scope.channelSelected = function($event, channel) {
-    chatService.setCurrentChannel(channel);
-    $scope.popover.hide();
+    if ($event.isIonicTap) {
+      chatService.setCurrentChannel(channel);
+      $scope.popover.hide();
+    }
   };
-  $rootScope.$on('currentChannelChanged', function (event, channel) {
+  $rootScope.$on('currentChannelChanged', function(event, channel) {
     $scope.subheader = channel.name;
   });
 });
 
 chatApp.controller('ContentController', function($rootScope, $scope, chatService) {
   $scope.messages = chatService.currentChannelMessages();
-  $rootScope.$on('newMessage', function (event, data) {
-      $scope.messages = chatService.currentChannelMessages();
+  $rootScope.$on('newMessage', function(event, data) {
+    $scope.messages = chatService.currentChannelMessages();
   });
-  $rootScope.$on('currentChannelChanged', function (event, channel) {
-      $scope.messages = chatService.currentChannelMessages();
+  $rootScope.$on('currentChannelChanged', function(event, channel) {
+    $scope.messages = chatService.currentChannelMessages();
   });
 });
 
